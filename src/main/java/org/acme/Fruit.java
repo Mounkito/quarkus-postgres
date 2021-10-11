8fa277cb81d92d9760e0eb3083e26a2fd43ebed5
Yohan <yohanlamerand@gmail.com>
2021-10-11 02:25:48 +0200

    first app quarkus postgres

---
+++ src/main/java/org/acme/Fruit.java
@@ -0,0 +1,26 @@
+package org.acme;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Cacheable
+public class Fruit extends PanacheEntity {
+
+    @Column(length = 40, unique = true)
+    public String name;
+
+    public Fruit() {
+    }
+
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+    public static Fruit findByName(String name){
+        return find("name", name).firstResult();
+    }
+}
